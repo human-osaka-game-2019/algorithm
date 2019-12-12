eded103120a02959324ed56cd74e0b5aa9d67c9c
mono1140 <11potato30@gmail.com>
2019-12-12 10:03:00 +0900

    :sperkles: Mapクラスの作成

---
+++ toda/Algorithm/Dijkstra/Map.cpp
@@ -0,0 +1,68 @@
+﻿#include "Map.h"
+
+Map::Map()
+{
+	// Initialize();
+}
+
+Map::~Map()
+{
+
+}
+
+void Map::DrawMapData(int MapData)
+{
+	switch (MapData)
+	{
+	case 0:
+		
+		cout << "　";
+		break;
+
+	case 1:
+
+		cout << "■";
+		break;
+
+	case 2:
+		
+		cout << "Ｓ" ;
+		break;
+
+	case 3:
+
+		cout << "Ｇ" ;
+		break;
+
+	default:
+		break;
+
+	}
+}
+
+void Map::Show()
+{
+	for (int a = 0; a < MapSize::Height; a++)
+	{
+		for (int b = 0; b < MapSize::Width; b++)
+		{
+			DrawMapData(Maze[a][b]);
+		}
+
+		cout << endl;
+	
+	}
+
+}
+
+void Map::Initialize()
+{
+	for (int a = 0; a < MapSize::Height; a++)
+	{
+		for (int b = 0; b < MapSize::Width; b++)
+		{
+			Maze[a][b] = 0;
+		}
+	}
+
+}
---
+++ toda/Algorithm/Dijkstra/Map.h
@@ -0,0 +1,49 @@
+﻿#ifndef MAP_H
+#define MAP_H
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+namespace MapSize
+{
+	const int Width = 11;
+	const int Height = 11;
+}
+
+class Map
+{
+public:
+
+	Map();
+
+	~Map();
+
+	int Maze[MapSize::Height][MapSize::Width] = 
+	{
+		{1,1,1,1,1,1,1,1,1,1,1},
+	    {1,2,0,0,0,0,0,0,0,0,1},
+	    {1,0,0,0,0,0,0,0,0,0,1},
+	    {1,0,0,0,0,0,0,0,0,0,1},
+	    {1,0,0,0,0,0,0,0,0,0,1},
+	    {1,0,0,0,0,0,0,0,0,0,1},
+	    {1,0,0,0,0,0,0,0,0,0,1},
+	    {1,0,0,0,0,0,0,0,0,0,1},
+	    {1,0,0,0,0,0,0,0,0,0,1},
+	    {1,0,0,0,0,0,0,0,0,3,1},
+	    {1,1,1,1,1,1,1,1,1,1,1}
+	
+	};
+
+	void Show();
+
+
+private:
+
+	void Initialize();
+
+	void DrawMapData(int MapDate);
+};
+
+#endif
