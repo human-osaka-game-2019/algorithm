14f12895df366572493d74c5b10435d41e4ea148
mono1140 <11potato30@gmail.com>
2019-12-19 10:44:24 +0900

    :sparkles: Main.cpp の追加

---
+++ toda/Algorithm/Dijkstra/Main.cpp
@@ -0,0 +1,24 @@
+﻿#include "Map.h"
+#include "Algorithm.h"
+
+int main()
+{
+	Map map;
+	Algorithm algo;
+
+	map.Show();
+
+	for (int a = 0; a < MapSize::Height; a++)
+	{
+		for (int b = 0; b < MapSize::Width; b++)
+		{
+			if (map.Maze[a][b] == 0)
+			{
+				algo.NodeCheck(map.maze, a, b);
+			}
+		}
+	}
+	
+	map.Show();
+
+}
