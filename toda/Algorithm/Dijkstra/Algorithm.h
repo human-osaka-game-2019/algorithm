b2142e8356ac17792cbf3389e50573440c09203d
mono1140 <11potato30@gmail.com>
2019-12-19 10:43:57 +0900

    :sparkles: Algorithm.h cpp を追加

---
+++ toda/Algorithm/Dijkstra/Algorithm.h
@@ -0,0 +1,54 @@
+﻿#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include "Map.h"
+#include <vector>
+
+enum class Direction
+{
+	Up,
+	Down,
+	Right,
+	Left
+};
+
+struct Road
+{
+	Road() : value(0)
+	{
+	}
+
+	int value;
+};
+
+class Node
+{
+public:
+
+	void InputPosition(int height, int width);
+	std::vector<int> Distance;
+
+private:
+
+	int Position[2];
+
+
+};
+
+class Algorithm
+{
+public:
+
+	void NodeCheck(int* map[],int height,int widht);
+
+
+private:
+
+	int NodeNumber = 0;
+
+	Road rode;
+	Node node[100];
+};
+
+
+#endif // !ALGORITHM_H
